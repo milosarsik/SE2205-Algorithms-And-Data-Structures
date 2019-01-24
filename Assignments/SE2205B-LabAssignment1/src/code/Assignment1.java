f53156b75c06fc03d6fc0cd3fcd6e00aa2ba49d6
Milos Arsik <marsik@uwo.ca>
2019-01-23 21:23:40 -0500

    Remade project

---
+++ Assignments/SE2205B-LabAssignment1/src/code/Assignment1.java
@@ -0,0 +1,41 @@
+package code;
+
+public class Assignment1
+{
+    // denseMatrixMult -
+    public int[][] denseMatrixMult(int[][] A, int[][] B, int size)
+    {
+        return new int[1][1];
+    }
+
+    // sum -
+    public int[][] sum(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
+    {
+        return new int[1][1];
+    }
+
+    // sub -
+    public int[][] sub(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
+    {
+        return new int[1][1];
+    }
+
+    // initMatrix -
+    public int[][] initMatrix(int n)
+    {
+        return new int[1][1];
+    }
+
+    // printMatrix -
+    public void printMatrix(int n, int[][] A)
+    {
+
+    }
+
+    // readMatrix -
+    public int[][] readMatrix(String filename, int n) throws Exception
+    {
+        return new int[1][1];
+    }
+}
+
