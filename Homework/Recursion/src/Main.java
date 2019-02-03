deb5ad9637a7ad56e9c9dee5376d70b79d4a97c8
Milos Arsik <marsik@uwo.ca>
2019-02-03 15:57:16 -0500

    restructured

--- Homework/Recursion/src/Main.java
+++ Homework/Recursion/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Queue;
+
 public class Main
 {
     public static void main(String[] args)
@@ -27,13 +29,16 @@ public class Main
         String temp = reverseString(problem517);
         System.out.println(temp);
 
-
-
     }
 
-    // problem 5.1 - Describe a recursive algorithm for finding the maximum element in an array, A, of n elements. What is your running time and space usage?
-        // runtime - O(n)
-            // space usage - ?
+    /**
+     * Problem 5.1 - Describe a recursive algorithm for finding the maximum element in an array, A,
+     * of n elements. What is your running time and space usage?
+     *
+     * @param   array   the array that is passed
+     * @param   n       size of the array
+     * @return          maximum element in the array
+     **/
     public static int findMaxElement(int[] array, int n)
     {
         // if size = 0, that means that the whole array has been traversed
@@ -45,9 +50,19 @@ public class Main
         return Math.max(array[n - 1], findMaxElement(array, n-1));
     }
 
-    // problem 5.2 - Explain how to modify the recursive binary search algorithm so that it returns the index of the target in the sequence or−1 (if the target is not found).
 
-    // problem 5.5 - Draw the recursion trace for the execution of reverseArray(data, 0, 4), from Code Fragment 5.7, on array data = 4, 3, 6, 2, 6.
+    /**
+     * Problem 5.2 - Explain how to modify the recursive binary search algorithm so
+     * that it returns the index of the target in the sequence or−1 (if the target is not found).
+     *
+     * Problem 5.5 - Draw the recursion trace for the execution of reverseArray(data, 0, 4), from
+     * Code Fragment 5.7, on array data = 4, 3, 6, 2, 6.
+     *
+     * @param   array   the array that is passed
+     * @param   low     low index
+     * @param   high    high index
+     * @return          reverse array
+     **/
     public static void reverseArray(int[] array, int low, int high)
     {
         // if at least two elements in subarray
@@ -61,7 +76,12 @@ public class Main
         }
     }
 
-    // problem 5.7 - Describe a recursive algorithm for computing the nth Harmonic number
+    /**
+     * Problem 5.7 - Describe a recursive algorithm for computing the nth Harmonic number
+     *
+     * @param   n   the nth harmonic number
+     * @return      computed nth harmonic number
+     **/
     public static double harmonicNumber(int n)
     {
         // base case, if n = 1 we have reached the lowest number that n can be, we know 1/1 is 1 therefore we return 1
@@ -73,6 +93,14 @@ public class Main
         return 1.0 / n + harmonicNumber(n - 1);
     }
 
+    /**
+     * Problem 5.10 - Describe a way to use recursion to compute the sum of all the
+     * elements in an n×n (two-dimensional) array of integers.
+     *
+     * @param   array   the 2D array we will compute
+     * @param   n       the size of the array
+     * @return          the computed sum of the 2D array
+     **/
     // problem 5.10 - Describe a way to use recursion to compute the sum of all the elements in an n×n (two-dimensional) array of integers.
     public static int sumOfAllElements(int [][] array, int n)
     {
@@ -93,7 +121,13 @@ public class Main
         return edges + sumOfAllElements(array, n - 1);
     }
 
-    // problem 5.17 - Write a short recursive Java method that takes a character string s and outputs its reverse. For example, the reverse of 'pots&pans' would be 'snap&stop'.
+    /**
+     * Problem 5.17 - Write a short recursive Java method that takes a character string
+     * s and outputs its reverse. For example, the reverse of 'pots&pans' would be 'snap&stop'.
+     *
+     * @param   string   the string to reverse
+     * @return           the reverse string
+     **/
     public static String reverseString(String string)
     {
         if(string.isEmpty())
@@ -104,10 +138,62 @@ public class Main
         return reverseString(string.substring(1)) + string.charAt(0);
     }
 
+    /**
+     * Problem 5.18 -  Write a short recursive Java method that determines if a string s is a
+     * palindrome, that is, it is equal to its reverse. Examples of palindromes include 'racecar'
+     * and 'gohangasalamiimalasagnahog'.
+     *
+     * @param   s   the sentence
+     * @return      whether the string is a palindrom or not
+     **/
+
+    /**
+     * Problem 5.21 -Given an unsorted array, A, of integers and an integer k, describe a recursive
+     * algorithm for rearrangingthe elements in A so that all elements less than or equal to k come
+     * before any elements larger than k. What is the running time of your algorithm on an array of
+     * n values?
+     *
+     * @param   A   unsorted array
+     * @param   k   integer
+     * @return      rearranged array
+     **/
+
+    /**
+     * Problem 5.23 - Describe a recursive algorithm that will check if an array A of integers
+     * contains an integer A[i] that is the sum of two integers that appear earlier in A, that is,
+     * such that A[i] = A[j]+A[k] for j,k < i.
+     *
+     * @param   A   array
+     * @return      returns whether there is an integer that is the sum of two integers earlier in A
+     **/
 
 
 
-    // printArray function
+    /**
+     * In-Class Problem - reverse a Queue
+     *
+     * @param   myQueue     the queue we want to reverse
+     * @return              the reverse queue
+     **/
+    public void reverseQueue(Queue<Integer> myQueue)
+    {
+        if(myQueue.isEmpty())
+        {
+            return;
+        }
+
+        int temp = myQueue.remove();
+
+        reverseQueue(myQueue);
+
+        myQueue.add(temp);
+    }
+
+    /**
+     * print function to print arrays
+     *
+     * @param   array   array that we will print
+     **/
     public static void printArray(int [] array, int size)
     {
         for(int i = 0; i < size; i++)
--- Labs/Lab1/src/milosarsik/se2205labs/Main.java
+++ Labs/Lab1/src/milosarsik/se2205labs/Main.java
@@ -4,13 +4,27 @@ import java.lang.Math;
 
 public class Main
 {
+    final static int[] S = {1, 5, 10, 20, 25};
+
     public static void main(String[] args)
     {
-        System.out.println(newtonSqrt(10,3,0.001));
+        System.out.println("Lab 1: Week of Jan 23, 2019");
 
+        System.out.println("Exericse 1");
+        System.out.println(newtonSqrt(4,2,0.001));
+
+        System.out.println("Exericse 2");
         System.out.println(findMinChangeCoins(1,1));
     }
 
+    /**
+     * Returns the square root of a given number to the precision
+     *
+     * @param   x   the number that we are finding the square root of
+     * @param   a   our best guess
+     * @param   eps precision
+     * @return      the square root of the number x
+     **/
     public static double newtonSqrt(double x, double a, double eps)
     {
         if( Math.abs((a*a) - x) <= eps)
@@ -25,8 +39,18 @@ public class Main
         }
     }
 
+    /** findMinChange - the coins that will be used are S = {1, 5, 10, 20, 25}
+     * @param   C   the change needed
+     * @param   n   the nth coin the set S containing the denominations of the coins in increasing order
+     * @return      the total least number of coins needed to make the change
+    **/
     public static int findMinChangeCoins(int C, int n)
     {
+        if(C == 0)
+        {
+            return 0;
+        }
+
         return 1;
     }
 }
