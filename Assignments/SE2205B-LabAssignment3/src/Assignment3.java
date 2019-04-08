7c1e7bbbdaa5d66f7ca4bdb98378781bccaece71
Cyriac Jinson <43037310+bat968@users.noreply.github.com>
2019-04-07 23:42:31 -0400

    Update

--- Assignments/SE2205B-LabAssignment3/src/Assignment3.java
+++ Assignments/SE2205B-LabAssignment3/src/Assignment3.java
@@ -2,102 +2,90 @@ import java.util.LinkedList;
 
 public class Assignment3
 {
-    static final int vert = 6;
+     int vertex = 6;
 
-    public int breadthFirstPathSearch(int graph[][], int s, int d, int parent[])       //vertices s and d
+    public int breadthFirstPathSearch(int graph[][], int s, int d, int parent[])//Breadth first path traversal
     {
-        int visited[] = new int[vert];
-
-        for(int i = 0; i < vert; ++i)
+        int reached[] = new int[vertex];
+        int count=0;
+        while(count < vertex)
         {
-            visited[i] = 0;
+            reached[count] = 0;
+            count++;
         }
 
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        queue.add(s);
-        visited[s] = 1;
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        q.add(s);
+        reached[s] = 1;
         parent[s]=-1;
 
-        while(queue.size() != 0)
+        while(q.size() != 0)
         {
-            int temp = queue.poll();
+            int temp = q.poll();
 
-            for(int v=0; v < vert; v++)
+            for(int v=0; v < vertex; v++)
             {
-                if(visited[v]==0 && graph[temp][v] > 0)
+                if(reached[v]==0 && graph[temp][v] > 0)
                 {
-                    queue.add(v);
+                    q.add(v);
                     parent[v] = temp;
-                    visited[v] = 1;
+                    reached[v] = 1;
                 }
             }
         }
 
-        if(visited[d] == 1)
+        if(reached[d] == 1)
         {
-            System.out.println("True");
+            System.out.println("T");
             return 1;
 
         }
         else
         {
-            System.out.println("False");
+            System.out.println("F");
             return 0;
         }
     }
 
-    public int maximizeFlowNetwork(int graph[][], int s, int t)     // computes maximum flow
+    public int maximizeFlowNetwork(int graph[][], int s, int t)    //max flow
     {
-        int u, v;
 
-        // Create a residual graph and fill the residual graph
-        // with given capacities in the original graph as
-        // residual capacities in residual graph
 
-        // Residual graph where rGraph[i][j] indicates
-        // residual capacity of edge from i to j (if there
-        // is an edge. If rGraph[i][j] is 0, then there is
-        // not)
-        int rGraph[][] = new int[vert][vert];
 
-        for (u = 0; u < vert; u++)
-            for (v = 0; v < vert; v++)
-                rGraph[u][v] = graph[u][v];
+        int resGraph[][] = new int[vertex][vertex];
+        int a, b;
+        for (a = 0; a < vertex; a++)
+            for (b = 0; b < vertex; b++)
+                resGraph[a][b] = graph[a][b];
+        int maxFlow = 0;
+        int parent[] = new int[vertex];
 
-        // This array is filled by BFS and to store path
-        int parent[] = new int[vert];
 
-        int max_flow = 0;  // There is no flow initially
 
-        // Augment the flow while tere is path from source
-        // to sink
-        while (breadthFirstPathSearch(rGraph, s, t, parent) == 1)
+
+        while (breadthFirstPathSearch(resGraph, s, t, parent) == 1)
         {
-            // Find minimum residual capacity of the edhes
-            // along the path filled by BFS. Or we can say
-            // find the maximum flow through the path found.
-            int path_flow = Integer.MAX_VALUE;
-            for (v=t; v!=s; v=parent[v])
+
+            int pathFlow = Integer.MAX_VALUE;
+            for (b=t; b!=s; b=parent[b])
             {
-                u = parent[v];
-                path_flow = Math.min(path_flow, rGraph[u][v]);
+                a = parent[b];
+                pathFlow = Math.min(pathFlow, resGraph[a][b]);
             }
 
-            // update residual capacities of the edges and
-            // reverse edges along the path
-            for (v=t; v != s; v=parent[v])
+
+            for (b=t; b != s; b=parent[b])
             {
-                u = parent[v];
-                rGraph[u][v] -= path_flow;
-                rGraph[v][u] += path_flow;
+                a = parent[b];
+                resGraph[a][b] =resGraph[a][b]- pathFlow;
+                resGraph[b][a] =resGraph[b][a]+ pathFlow;
             }
 
-            // Add path flow to overall flow
-            max_flow += path_flow;
+            maxFlow = maxFlow+ pathFlow;
         }
 
-        // Return the overall flow
-        return max_flow;
+
+        return maxFlow;
 
     }
 
